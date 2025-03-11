922b80101ac54ef05a78046c3f09bd6376c24c81
rebix111 <rebix1678@gmail.com>
2025-03-11 22:03:51 +0100

    Update gra.py
    dodałem menu startowe ;0

--- projekt_gry/gra.py
+++ projekt_gry/gra.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-
+from tkinter import Tk , Button
 pygame.init()
 pygame.mixer.init()
 
@@ -15,7 +15,7 @@ rekord_rund_czerwonego = 0
 kolor_bialy = (255, 255, 255)
 kolor_czorny = (0, 0, 0)
 kolor_czerwont = (255, 0, 0)
-zuty_kolor = (255, 255, 0)
+zulty_kolor = (255, 255, 0)
 
 przesuniencie = 5
 wysokosc_statku, szerokosc_statku = 55, 40
@@ -23,47 +23,49 @@ poruszanie_pociskow = 14
 border = pygame.Rect(szerokosc_okna / 2 - 5, 0, 10, wysokosc_okna)
 narysuj_zdrowie = pygame.font.SysFont('comicsans', 30)
 max_naboje = 3
-zuty_udezony = pygame.USEREVENT + 1
+zulty_udezony = pygame.USEREVENT + 1
 czerwony_udezony = pygame.USEREVENT + 2
 
-zdrowie_zultego = 10
-zdrowie_czerwonego = 10
-
-base_path = os.path.dirname(__file__)
-
-zuty_statek = pygame.image.load(
-    os.path.join(base_path, 'tekstury', 'spaceship_yellow.png'))
-zuty_statek = pygame.transform.rotate(pygame.transform.scale(zuty_statek, (wysokosc_statku, szerokosc_statku)), 90)
-
-czerwony_statek = pygame.image.load(
-    os.path.join(base_path, 'tekstury', 'spaceship_red.png'))
-czerwony_statek = pygame.transform.rotate(pygame.transform.scale(czerwony_statek, (wysokosc_statku, szerokosc_statku)), 270)
-
-dziwienk_pocisku = pygame.mixer.Sound(os.path.join(base_path, 'dziwięki', 'Grenade+1.mp3'))
-ogien_pocisku = pygame.mixer.Sound(os.path.join(base_path,'dziwięki','Gun+Silencer.mp3'))
-sound_track = pygame.mixer.Sound(os.path.join(base_path,'dziwięki','pixel-perfect-112527.mp3'))
-
+dziwienk_pocisku = pygame.mixer.Sound('projekt_gry\\dziwięki\\Grenade+1.mp3')
+ogien_pocisku = pygame.mixer.Sound('projekt_gry\\dziwięki\\Gun+Silencer.mp3')
+sound_track = pygame.mixer.Sound('projekt_gry\\dziwięki\\pixel-perfect-112527.mp3')
+przejscie_sound = pygame.mixer.Sound('projekt_gry\\dziwięki\\332003_lloydevans09_whoosh (online-audio-converter.com).mp3')
 dziwienk_pocisku.set_volume(0.1)
 ogien_pocisku.set_volume(0.1)
 sound_track.set_volume(0.1)
+przejscie_sound.set_volume(0.5)
+
+zdrowie_zultego = 10
+zdrowie_czerwonego = 10
+pixel_font = pygame.font.Font('projekt_gry\\inne\\PressStart2P-Regular.ttf', 25)
+zuty_statek = pygame.image.load(
+    os.path.join('projekt_gry','tekstury', 'spaceship_yellow.png'))
+zuty_statek = pygame.transform.rotate(pygame.transform.scale(zuty_statek, (wysokosc_statku, szerokosc_statku)), 90)
+
+czerwony_statek = pygame.image.load(
+    os.path.join('projekt_gry','tekstury', 'spaceship_red.png'))
+czerwony_statek = pygame.transform.rotate(pygame.transform.scale(czerwony_statek, (wysokosc_statku, szerokosc_statku)), 270)
 
 tlo = pygame.image.load(
-    os.path.join(base_path, 'tekstury', 'space.png'))
+    os.path.join('projekt_gry','tekstury', 'space.png'))
 
-base_path = os.path.dirname(__file__)
-rounds_file_path = os.path.join(base_path, 'projekt_gry', 'rounds.txt')
+przycisk = pygame.image.load(
+    os.path.join('projekt_gry','tekstury', 'obraz_2025-03-11_202421195-removebg-preview.png'))
+przycisk = pygame.transform.scale(przycisk, (300,150))
+
+tlo_menu = pygame.image.load(os.path.join('projekt_gry','tekstury','Sprite.jpg'))
+tlo_menu = pygame.transform.scale(tlo_menu, (900,500))
 def wczytaj_liczbe_rund():
-    if not os.path.exists(rounds_file_path):
-        os.makedirs(os.path.dirname(rounds_file_path), exist_ok=True)
-        with open(rounds_file_path, 'w') as file:
+    if not os.path.exists('projekt_gry/rounds.txt'):
+        with open('projekt_gry/rounds.txt', 'w') as file:
             file.write('0')
         return 0
     else:
         try:
-            with open(rounds_file_path, 'r') as file:
+            with open('projekt_gry/rounds.txt', 'r') as file:
                 rounds = file.read().strip()
-                return int(rounds)
-        except ValueError:
+                return int(rounds) 
+        except ValueError:  
             print("Błąd wczytywania liczby rund, ustawiono 0")
             return 0
 
@@ -72,10 +74,12 @@ def zapisz_liczbe_rund(liczba_rund):
         file.write(str(liczba_rund))
 
 def wyswietl_tekst(tekst, kolor, x, y):
-    czcionka = pygame.font.SysFont("comicsans", 70)
+    czcionka = pygame.font.SysFont("comicsans", 30)
     tekst_obiekt = czcionka.render(tekst, True, kolor)
     okno.blit(tekst_obiekt, (x, y))
-
+def wyswietl_tekst_pixelowy(tekst, kolor, x, y):
+    tekst_obiekt = pixel_font.render(tekst, True, kolor)
+    okno.blit(tekst_obiekt, (x, y))
 def okno_config(czerwony, zuty, zuty_pociski, czerwony_pociski, zdrowie_czerwonego, zdrowie_zultego, liczba_rund):
     okno.blit(tlo, (0, 0))
     pygame.draw.rect(okno, kolor_czorny, border)
@@ -87,6 +91,7 @@ def okno_config(czerwony, zuty, zuty_pociski, czerwony_pociski, zdrowie_czerwone
     okno.blit(zdrowie_czerwonego_tekst, (szerokosc_okna - zdrowie_czerwonego_tekst.get_width() - 10, 10))
     okno.blit(zdrowie_zuletego_tekst, (10, 10))
     okno.blit(liczba_rund_tekst, (szerokosc_okna / 2 - liczba_rund_tekst.get_width() / 2, 10))
+
     okno.blit(zuty_statek, (zuty.x, zuty.y))
     okno.blit(czerwony_statek, (czerwony.x, czerwony.y))
 
@@ -94,7 +99,7 @@ def okno_config(czerwony, zuty, zuty_pociski, czerwony_pociski, zdrowie_czerwone
         pygame.draw.rect(okno, kolor_czerwont, pocisku)
 
     for pocisku in zuty_pociski:
-        pygame.draw.rect(okno, zuty_kolor, pocisku)
+        pygame.draw.rect(okno, zulty_kolor, pocisku)
     pygame.display.update()
 
 def kolizja_pocisku(zuty_pociski, czerwony_pociski, zuty, czerwony):
@@ -108,7 +113,7 @@ def kolizja_pocisku(zuty_pociski, czerwony_pociski, zuty, czerwony):
     for pocisk in czerwony_pociski:
         pocisk.x -= poruszanie_pociskow
         if zuty.colliderect(pocisk):
-            pygame.event.post(pygame.event.Event(zuty_udezony))
+            pygame.event.post(pygame.event.Event(zulty_udezony))
             czerwony_pociski.remove(pocisk)
         elif pocisk.x < 0:
             czerwony_pociski.remove(pocisk)
@@ -151,7 +156,9 @@ def main():
 
     dziala = True
     czas = pygame.time.Clock()
+
     sound_track.play(-1)
+
     while dziala:
         czas.tick(60)
 
@@ -172,7 +179,7 @@ def main():
             if event.type == czerwony_udezony:
                 zdrowie_czerwonego -= 1
                 ogien_pocisku.play()
-            if event.type == zuty_udezony:
+            if event.type == zulty_udezony:
                 zdrowie_zultego -= 1
                 ogien_pocisku.play()
 
@@ -191,15 +198,37 @@ def main():
         if tekst_wygranej != "":
             wyswietl_tekst(tekst_wygranej, kolor_bialy, szerokosc_okna / 2 - 150, wysokosc_okna / 2 - 30)
             pygame.display.update()
-            pygame.time.delay(2000)
-            czerwony, zuty, zuty_pociski, czerwony_pociski = reset_gra()
+            pygame.time.delay(2000)  
+            czerwony, zuty, zuty_pociski, czerwony_pociski = reset_gra()  
         kolizja_pocisku(zuty_pociski, czerwony_pociski, zuty, czerwony)
 
         przycisk = pygame.key.get_pressed()
         zuty_movment(przycisk, zuty)
         czerwony_movment(przycisk, czerwony)
         okno_config(czerwony, zuty, zuty_pociski, czerwony_pociski, zdrowie_czerwonego, zdrowie_zultego, liczba_rund)
+def menu_startowe():
+    sound_track.play(-1)
+    przycisk_rect = przycisk.get_rect(topleft=(300, 200))  
 
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if przycisk_rect.collidepoint(event.pos):  
+                    sound_track.stop()
+                    przejscie_sound.play()
+                    return 
+
+
+        okno.blit(tlo_menu, (0,0))
+        wyswietl_tekst_pixelowy("kliknij start zeby wystartowac", kolor_bialy, 90, 100)
+        okno.blit(przycisk, przycisk_rect.topleft)  
+
+        pygame.display.update()
+    
 if __name__ == "__main__":
+    menu_startowe()
     main()
